e79b47ada27795ec26ab71d2bf778e8730a3b7f8
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2022-02-03 19:57:13 +0100

    Create examen.java

---
+++ examen.java
@@ -0,0 +1,182 @@
+/**
+*     Importar .txt y .bin
+*/
+public void importarTexto() throws IOException{
+  Path ruta = Rutas.pathToFileInFolderOnDesktop(namefolder,namefiletxt);
+  File file = ruta.toFile();
+  if(file.exists()){
+    try{
+      ArrayList<String> lineas = (ArrayList<String>) readAllLines(ruta);
+      for(String i : lineas){
+        String[] campos = i.split(delim);
+        Objeto objeto = Objeto.factory(campos);
+        if(objeto!=null){
+          listaObjetos.add(objeto);
+        }else{
+          System.err.printf("Error.%n");
+        }
+      }
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }else{
+    System.err.printf("Error.%n");
+  }
+}
+
+public void importarBin(){
+  Path ruta =Rutas.pathToFileInFolderOnDesktop(namefolder,namefilebin);
+  File file = ruta.toFile();
+  try{
+    FileInputStream fis = new FileInputStream(file);
+    BufferedInputStream bis = new BufferedInputStream(fis);
+    ObjectInputStream ois = new ObjectInputStream(bis);
+    listaObjetos = (ArrayList<String>) ois.readObject();
+    ois.close();
+  }catch(IOException | ClassNotFoundException ex){
+    ex.printStackTrace();
+  }
+}
+
+
+/**
+*     Exportar .txt y .bin
+*/
+public void exportarTexto(){
+  Path ruta = Rutas.pathToFileInFolderOnDesktop(namefolder,namefiletxt);
+  File file = ruta.toFile();
+  String[][] data = new String[listaObjetos.size()][];
+  for(int i=0;i<listaObjetos.size();i++){
+    data[i] = listaObjetos.get(i).toString.split(delim);
+  }
+  try{
+    exportToDisk(data,file,delim);
+  }catch(IOException e){
+    e.printStackTrace();
+  }
+}
+
+public void exortarBinario(){
+  Path ruta = Rutas.pathToFileInFolderOnDesktop(namefolder,namefilebin);
+  File file = ruta.toFile();
+  try{
+    FileOutputStream fos = new FileOutputStream(file);
+    BufferedOutputStream bos = new BufferedOutputStream(fos);
+    ObjectOutputStream oos = new ObjectOutputStream(bos);
+    oos.writeObject(listaObjetos);
+    oos.close();
+  }catch(IOException e){
+    e.printStackTrace();
+  }
+}
+
+/**
+*     Mapas
+*/
+public void prepararMapa(){
+  for(Objeto obj : listaObjetos){
+    mapa.put(obj.getAtr2,obj);
+  }
+}
+
+public String datosComoStringDelimMapa(String Atr2, String delim){
+  Objeto obj = mapa.get(Atr2);
+  String token = obj.getAtr1)+delim+obj.getAtr2()+delim+obj.getAtr3)+delim;
+  return token;
+}
+
+/**
+*     Modificar o Consultar
+*/
+public void modificar(String Atr1, String newAtr1){
+  for(Objeto obj : listaObjetos){
+    if(obj.getAtr1().equalsIgnoreCase(Atr1)){
+      obj.setAtr1(newAtr1);
+    }
+  }
+}
+
+public String consultar(String Atr1){
+  String consulta = "";
+  for(Objeto obj : listaObjetos){
+    if(obj.getAtr1().equalsIgnoreCase(Atr1)){
+      consulta=obj.getAtr1()+" "+obj.getAtr2()+" "+obj.getAtr3()+" ";
+      for(String caracteristicas :  objeto.caracteristicas){
+        consulta=consulta+carecteristicas+"\n";
+      }
+    }
+  }
+  return consulta;
+}
+
+/**
+*     Ordenar
+*/
+public void ordenar(){
+  listaObjetos.sort(Comparator.comparing(Objeto::getAtr1).thenComparing(Objeto::getAtr2).thenComparing(Objeto::getAtr3));
+}
+
+/**
+*     ToString y Mostrar por pantalla
+*/
+public void toString(){
+	return atr1+"#"+atr2+"#"+atr3+"#"+atr4+"#";
+}
+public void toString(){
+	String data = "";
+	data = o.getAtr1()+"#"+o.getAtr2()+"#"+o.Atr3+"#"+o.Atr4+"#";
+	for(String i : Atr5){
+		data = data +" "+i;
+	}
+}
+
+public String[] estadoComoArrayDeCadenas(){
+	String[] linea = null;
+	linea[0] = this.nombre;
+	linea[1] = this.ap1;
+    linea[2] = this.ap2;
+	linea[3] = String.valueOf(this.edad);
+	linea[4] = String.valueOf(this.talla);
+	linea[5] = String.valueOf(this.peso);
+	return linea;
+}
+
+public void ArrayListToMatrix(){
+	String[][] data = new String[listaObjetos.size()][];
+	for(int i=0;i<listaObjetos.size();i++){
+		data[i] = listaObjetos.get(i).toString().split("#");
+	}
+	return data;
+  printToScreen3(data);
+}
+
+
+/**
+*     Factory
+*/
+public static Objeto factory(String linea, String delim){
+	String[] campos = linea.split(delim);
+	try{
+		int tok1 = Integer.parseInt(campos[4]);
+		float tok2 = Float.parseFloat(campos[5]);
+		double tok3 = Double.parseDouble(campos[6]);
+		return new Objeto(campos[0],campos[1],campos[2],campos[3],tok1,tok2,tok3);
+	}catch(NumberFormatException e){
+		e.printStackTrace();
+		return null;
+	}
+}
+public static Objeto factory(String[] campos){
+	try{
+		int tok1 = Integer.parseInt(campos[4]);
+		float tok2 = Float.parseFloat(campos[5]);
+		double tok3 = Double.parseDouble(campos[6]);
+		return new Objeto(campos[0],campos[1],campos[2],campos[3],tok1,tok2,tok3);
+	}catch(NumberFormatException e){
+		e.printStackTrace();
+		return null;
+	}	
+}
+
+
+
